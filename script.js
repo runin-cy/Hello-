5da769e8fc70465e214b7dbf368787dc9cb9cf86
runin-cy <71084530+runin-cy@users.noreply.github.com>
2020-09-10 10:26:28 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let firstName = "Renee";
+
+let lastName = "Szatkiewicz";
+
+// String
+// "Renee" "Szatkiewicz"
+
+let age = 31;
+
+alert("My name is Renee Szatkiewicz");
+
+alert("I am 31 years old");
+
+let olderthan = true;
+
+if(olderthan){
+    alert("I am legally able to drink in the states")
+}
+else if(exactly){
+    alert("I am just barely able to drink legally in the states")
+}
+else if(under){
+    alert("It is illegal for me to drink in the states")
+}
+
+console.log("I am actually having fun learning about how to use Javascript");
